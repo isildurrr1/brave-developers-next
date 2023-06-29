f989659b83e37ff44b76e054c98989bba9b8b692
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-29 21:13:23 +0700

    id routing

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,25 +1,49 @@
 // import Form from "@/components/Form"
-import { usePathname } from "next/navigation";
-import { useRouter } from 'next/router'
+'use client'
 import '@/app/page.sass'
 import { apiOperators } from '@/data/api'
 import OperatorElement from "@/components/OperatorElement";
-import { IElement } from "@/types/types";
-import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { IMaskInput } from 'react-imask';
 
-const Operator = ({lol}: IElement) => {
-  console.log(lol)
-  return(
+interface Props {
+  params: {
+    id: number
+  }
+}
+
+type Inputs = {
+  phone: string
+}
+
+export const Operator = ({ params }: Props) => {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({ mode: "all" });
+  const navigate = useRouter()
+  const onSubmit = (data: any) => {
+    console.log(data.phone.length)
+  }
+  return (
     <div className="App">
-      <h1 className="title">lol</h1>
-      <OperatorElement data={apiOperators[0]}/>
+      <h1 className="title">Введите данные</h1>
+      <OperatorElement data={apiOperators[params.id]} />
+      <form onSubmit={handleSubmit(onSubmit)} action="#" className="form">
+        <IMaskInput className="input"
+          noValidate
+          mask={"{+7}(000)000-00-00"}
+          unmask={true}
+          placeholder='Номер телефона'
+          {...register("phone", {
+            required: "Error",
+            minLength: { value: 17, message: "Error222" }
+          })}
+        />
+        {errors?.phone && <span>{errors.phone.message}</span>}
+        <button type="submit" className='button submit' name='submitButton'>Пополнить</button>
+        <button onClick={() => navigate.push('/')} className='button backButton'>Назад</button>
+      </form>
     </div>
   )
 }
 export default Operator
 
-Operator.getServerSideProps = async (context: GetServerSidePropsContext): Promise<{ props: IElement }> => {
-  const lol = apiOperators[0]
-  console.log(context)
-  return { props: { lol } };
-}
